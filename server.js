d3cc7470d8a7a9e511113f093df8e757b34fc5b5
truemail785 <truemail785@gamil.com>
2021-02-19 11:52:30 +0500

    jfdk

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 process.env.MONGO_URI="mongodb://ozair_ayaz:ozair_03235146562@cluster0-shard-00-00.m4fyc.mongodb.net:27017,cluster0-shard-00-01.m4fyc.mongodb.net:27017,cluster0-shard-00-02.m4fyc.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-92jndn-shard-0&authSource=admin&retryWrites=true&w=majority"
 process.env.FACEBOOK_CLIENT_ID="203742888108497";
 process.env.FACEBOOK_CLIENT_SECRET="642045368df770d3a3d2b4028788aad8";
-process.env.FACEBOOK_CALLBACK_URL="http://localhost:5000/api/user/auth/facebook/callback";
+process.env.FACEBOOK_CALLBACK_URL="https://ozauth.herokuapp.com//api/user/auth/facebook/callback";
 process.env.GOOGLE_CLIENT_ID="893366708424-hqn0as5nnr3u05pesd8n7mnls4gonsa6.apps.googleusercontent.com";
 process.env.GOOGLE_CLIENT_SECRET="gFyF5mPo8RCF31gtclbtUNia";
-process.env.GOOGLE_CALLBACK_URL="http://localhost:5000/api/user/auth/google/callback";
-process.env.GITHUB_CLIENT_ID="203c7f1074e887b5f24f"
-process.env.GITHUB_CLIENT_SECRET="3c79919edab3890b0050ab4647b828de3f6e44f2"
-process.env.GITHUB_CALLBACK_URL="http://localhost:5000/api/user/auth/github/callback"
+process.env.GOOGLE_CALLBACK_URL="https://ozauth.herokuapp.com//api/user/auth/google/callback";
+process.env.GITHUB_CLIENT_ID=" 968ba55ef1b55b634023 "
+process.env.GITHUB_CLIENT_SECRET="5ff7724e9a9ce79f83fdbd497db4609d6868d1a4"
+process.env.GITHUB_CALLBACK_URL="https://ozauth.herokuapp.com//api/user/auth/github/callback"
 const express = require("express")
 const api = require("./routes/api")
 const bodyParser = require("body-parser")
